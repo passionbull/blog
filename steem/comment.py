d54e119b9ffbf152e9d9a27404af86b309fc5704
Jacob <wnsl5684@gmail.com>
2019-10-16 17:40:58 +0900

    ignore category that is integer

--- steem/comment.py
+++ steem/comment.py
@@ -57,18 +57,16 @@ class SteemComment:
     def get_category(self):
         c = self.get_comment()
         if c.json_metadata and 'category' in c.json_metadata:
-            if type(c.json_metadata['category']) == type([]):
-                return c.json_metadata['category']
-            # elif type(c.json_metadata['category']) == type(''):
-            #     return [c.json_metadata['category']]
+            if type(c.json_metadata['category']) == type([]) and len(c.json_metadata['category']) > 0:
+                if type(c.json_metadata['category'][0]) != type(0): # category type is not int
+                    return c.json_metadata['category']
         else:
             self.refresh()
             c = self.get_comment()
             if c.json_metadata and 'category' in c.json_metadata:
-                if type(c.json_metadata['category']) == type([]):
-                    return c.json_metadata['category']
-                # elif type(c.json_metadata['category']) == type(''):
-                #     return [c.json_metadata['category']]
+                if type(c.json_metadata['category']) == type([]) and len(c.json_metadata['category']) > 0:
+                    if type(c.json_metadata['category'][0]) != type(0): # category type is not int
+                        return c.json_metadata['category']
         return []
 
     def get_tags(self):
